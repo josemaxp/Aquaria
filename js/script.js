23f631425e9ca42b9f4711667ea09b732881a947
José Manuel Marín <josemaxp@gmail.com>
2023-01-13 00:01:13 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 function calculateLuminosity(){
   var liters = document.getElementById("litros").value;
   var watts = document.getElementById("watts").value;
@@ -27,16 +29,12 @@ function hideTemperature(){
 
 function loadFishData(){
 
-  fetch("../json/fish.json")
-  .then(function (response){
-    return response.json();
-  })
-  .then(function (data){
-    for(var i = 0; i<data.records.length; i++){
-      document.getElementById("info").innerHTML += "<div class='swiper-slide card'><h5>"+data.records[i].NOMBRE+"</h5>"+
-      "<div class='pra'><p class='cvtext' >"+data.records[i].NOMBRE+"</p></div></div>";
-    }
-  })
+
+  for(var i = 0; i<data.records.length; i++){
+    document.getElementById("info").innerHTML += "<div class='swiper-slide card'><h5>"+data.records[i].NOMBRE+"</h5>"+
+    "<div class='pra'><p class='cvtext' >"+data.records[i].NOMBRE+"</p></div></div>";
+  }
+  
 }
 
 var swiper = new Swiper(".mySwiper", {
@@ -54,3 +52,7 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+/* JSON */
+
+const data = JSON.parse('{"records": [{"id": 1,"NOMBRE": "CERRO DEL ÁGUILA","LONGITUD": "4 kms","TIEMPO DE IDA": "1:30 h.","DIFICULTAD": "Baja","LONGITUDE": "-6.3196114","LATITUDE": "36.848971","DESCRIPCION": "Desde Sanlúcar de Barrameda tomar hacia Bonanza, en dirección norte la CA-9027. Pasada La Algaida, a unos 100 metros, se inicia el sendero.","IMAGEN": "https://www.juntadeandalucia.es/medioambiente/portal/documents/255035/5656070/170_SS_03.jpg"},{"id": 2,"NOMBRE": "LAGUNAS DE ESPERA","LONGITUD": "2 kms","TIEMPO DE IDA": "45 min.","DIFICULTAD": "Baja","LONGITUDE": "-5.8649104","LATITUDE": "36.8730955","DESCRIPCION": "El acceso a la Reserva Natural Lagunas de Espera se puede realizar por dos lugares diferentes. El primer acceso lo tenemos en la parte más alta del municipio de Espera, junto al cementerio y muy próximo a su castillo medieval. Desde allí parte un carril descendiente que nos llevará directamente a las lagunas, siguiendo las indicaciones que nos encontremos. El otro acceso lo tenemos en la carretera CA 4412/SE 447 Espera-Las Cabezas de San Juan, a la que llegamos desde el pueblo, dirección Sevilla. A un 1.5 Km. de la localidad por esta carrtera tenemos señalizada la entrada a un carril que nos llevará al camino antes descrito, de unos 5Km.","IMAGEN": "https://www.juntadeandalucia.es/medioambiente/portal/documents/255035/5678990/1067_SS_01.jpg"}]}');
