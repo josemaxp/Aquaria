2ef76b8e20aab0dc0b3782ecc99eb652c68e4624
José Manuel Marín <josemaxp@gmail.com>
2023-01-12 23:08:00 +0100

    update

--- js/script.js
+++ js/script.js
@@ -24,3 +24,33 @@ function hideTemperature(){
     x.style.display = "none";
   }
 }
+
+function loadFishData(){
+
+  fetch("https://josemaxp.github.io/jsonapi/senderoscadiz.json")
+  .then(function (response){
+    return response.json();
+  })
+  .then(function (data){
+    for(var i = 0; i<data.records.length; i++){
+      document.getElementById("info").innerHTML += "<div class='swiper-slide card'><h5>"+data.records[i].NOMBRE+"</h5>"+
+      "<div class='pra'><p class='cvtext' >"+data.records[i].NOMBRE+"</p></div></div>";
+    }
+  })
+}
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetweenSlides: 30,
+  slidesPerGroup: 3,
+  loop: false,
+  loopFillGroupWithBlank: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
